d1ad61d5f299061431dbf7ac501cc8cb21545a9f
stevegomes <sgomes@mobiquityinc.com>
2016-02-03 16:37:50 +0530

    again some change

--- src/testPackage1/sampleprint.java
+++ src/testPackage1/sampleprint.java
@@ -2,7 +2,7 @@ package testPackage1;
 //wap to print the multiple of 3
 public class sampleprint {
 
-    # Some changed in code
+    // changed as suggested
 	public static void main(String[] args) {
 		for(int i=0;i<=10;i++)
 		{
